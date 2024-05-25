06e26c4a9f037a7afa47bbfa0a63885a7e38f293
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2024-05-25 17:28:06 -0300

    Adding project files

---
+++ src/main/java/com/ecritic/ecritic_authentication_service/ECriticAuthenticationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ecritic.ecritic_authentication_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECriticAuthenticationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECriticAuthenticationServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecritic/ecritic_authentication_service/ECriticAuthenticationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecritic.ecritic_authentication_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECriticAuthenticationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
