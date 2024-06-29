7bb4949e5df607f546d851fd163c5c80ccb04b45
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2024-06-29 20:35:23 -0300

    Adding feign dependency

--- src/main/java/com/ecritic/ecritic_authentication_service/ECriticAuthenticationServiceApplication.java
+++ src/main/java/com/ecritic/ecritic_authentication_service/ECriticAuthenticationServiceApplication.java
@@ -2,12 +2,14 @@ package com.ecritic.ecritic_authentication_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class ECriticAuthenticationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ECriticAuthenticationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ECriticAuthenticationServiceApplication.class, args);
+    }
 
 }
