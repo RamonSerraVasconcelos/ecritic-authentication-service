dc0a13eb2d63fcdfa06f3bfb4d4942cd739eeaac
Ramon Vasconcelos <rserravasconcelos@gmail.com>
2024-07-06 22:19:52 -0300

    Changing UserEntity response name

--- src/main/java/com/ecritic/ecritic_authentication_service/dataprovider/api/mapper/UserEntityMapper.java
+++ src/main/java/com/ecritic/ecritic_authentication_service/dataprovider/api/mapper/UserEntityMapper.java
@@ -1,11 +1,11 @@
 package com.ecritic.ecritic_authentication_service.dataprovider.api.mapper;
 
 import com.ecritic.ecritic_authentication_service.core.model.User;
-import com.ecritic.ecritic_authentication_service.dataprovider.api.entity.UserEntity;
+import com.ecritic.ecritic_authentication_service.dataprovider.api.entity.UserEntityResponse;
 import org.mapstruct.Mapper;
 
 @Mapper(componentModel = "spring")
 public interface UserEntityMapper {
 
-    User userEntityToUser(UserEntity userEntity);
+    User userEntityToUser(UserEntityResponse userEntityResponse);
 }
